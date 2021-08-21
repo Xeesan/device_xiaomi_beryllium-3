4031cbc5cf755b794e4bb8c3bdfa2c684ddeb14f
Xisan <82621427+Xeesan@users.noreply.github.com>
2021-08-21 14:11:51 +0600

    rrify :)

--- rr_beryllium.mk
+++ rr_beryllium.mk
@@ -9,16 +9,16 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Corvus stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
+# RR Stuffs
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := du_beryllium
+PRODUCT_NAME := rr_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# RR Stuffs
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
 
